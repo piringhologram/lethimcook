606fff91b39a1c997a4b91a61656b9c4999ffb45
Christian Devin <christian.devin@tum.de>
2024-10-09 16:39:56 +0200

    add a mock model to support preview on recipe view

---
+++ lethimcook/lethimcook/AppModel/MockModel.swift
@@ -0,0 +1,20 @@
+//
+//  MockModel.swift
+//  lethimcook
+//
+//  Created by ipraktikum on 09.10.24.
+//
+
+import Foundation
+
+public class MockModel: Model {
+    public convenience init() {
+        let recipeOne = Recipe(id: UUID(), title: "Fried Egg", ingredients: "Egg and Stuff", instructons: "Add egg then cook")
+        let recipeTwo = Recipe(id: UUID(), title: "Boiled Egg", ingredients: "Egg and Pepper", instructons: "Add egg then boil")
+        let recipeThree = Recipe(id: UUID(), title: "Sunny Side Egg", ingredients: "Salt and Egg", instructons: "Add egg then sun")
+        
+        let recipes = [recipeOne, recipeTwo, recipeThree]
+        
+        self.init(recipes: recipes)
+    }
+}
